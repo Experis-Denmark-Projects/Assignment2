09ebb9df5e6e66704e9bfd237ad2366df255c159
Alexander <91903660+AlexanderJonstrup@users.noreply.github.com>
2023-08-21 09:26:03 +0200

    Update 02_RelationshipSuperheroAssistant.sql
    Removed SuperheroPowers linking table code.

--- 02_RelationshipSuperheroAssistant.sql
+++ 02_RelationshipSuperheroAssistant.sql
@@ -1,7 +1,1 @@
 ALTER TABLE Superheroes ADD ass_id serial REFERENCES ASSISTANT;
-
-CREATE TABLE SuperheroPowers (
-	sh_id serial REFERENCES Superheroes,
-	power_id serial REFERENCES Power,
-	PRIMARY KEY (sh_id, power_id)
-);
