1bf431b7c395fb36c5a1c8338b7568657c373200
Alexander <91903660+AlexanderJonstrup@users.noreply.github.com>
2023-08-21 10:56:19 +0200

    Create 06_powrs.sql
    Added data to Power table.

---
+++ 06_powrs.sql
@@ -0,0 +1,7 @@
+INSERT INTO Power VALUES (1, 'Money', 'Has a lot of money');
+
+INSERT INTO Power VALUES (2, 'Super speed', 'Is very fast');
+
+INSERT INTO Power VALUES (3, 'Gadget', 'Has many gadgets');
+
+INSERT INTO Power VALUES (4, 'Super strength', 'Is very strong');
